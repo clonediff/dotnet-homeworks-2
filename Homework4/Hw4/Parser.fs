5a6ad3fb2c3bad3a48a2ed03a6cfb2bf9da4ffbe
clonediff <32036592+clonediff@users.noreply.github.com>
2022-10-04 23:11:41 +0300

    [add] HW4

--- Homework4/Hw4/Parser.fs
+++ Homework4/Hw4/Parser.fs
@@ -2,6 +2,7 @@
 
 open System
 open Hw4.Calculator
+open Microsoft.FSharp.Core
 
 
 type CalcOptions = {
@@ -11,10 +12,25 @@ type CalcOptions = {
 }
 
 let isArgLengthSupported (args : string[]) =
-    NotImplementedException() |> raise
+    args.Length = 3
 
 let parseOperation (arg : string) =
-    NotImplementedException() |> raise
+    match arg with
+    | "+" -> CalculatorOperation.Plus
+    | "-" -> CalculatorOperation.Minus
+    | "*" -> CalculatorOperation.Multiply
+    | "/" -> CalculatorOperation.Divide
+    | _ -> CalculatorOperation.Undefined
+
+let parseCalcArguments(args : string[]) = 
+    if (isArgLengthSupported(args) |> not) then
+        ArgumentException() |> raise
+        
+    let val1 = try float args[0] with _ -> ArgumentException() |> raise
+    let val2 = try float args[2] with _ -> ArgumentException() |> raise
+    let oper = parseOperation args[1]
     
-let parseCalcArguments(args : string[]) =
-    NotImplementedException() |> raise
+    if (oper = CalculatorOperation.Undefined) then
+        ArgumentException() |> raise
+    
+    {arg1 = val1; operation = oper; arg2 = val2}
