eca22c2013feaebf18af72a730193660035e3592
clonediff <32036592+clonediff@users.noreply.github.com>
2022-10-07 12:11:17 +0300

    [add] HW4

--- Homework4/Hw4/Program.fs
+++ Homework4/Hw4/Program.fs
@@ -1,1 +1,11 @@
-﻿raise (System.NotImplementedException())
+﻿open System.Diagnostics.CodeAnalysis
+open Hw4.Parser
+open Hw4.Calculator
+
+[<ExcludeFromCodeCoverage>]
+[<EntryPoint>]
+let Main args =
+    let parsedArgs = parseCalcArguments args
+    let result = calculate parsedArgs.arg1 parsedArgs.operation parsedArgs.arg2
+    printfn $"{result}"
+    0
