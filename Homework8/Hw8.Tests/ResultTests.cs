888c3de3a3e5685fa19f344c29faabadcd2189a4
clonediff <32036592+clonediff@users.noreply.github.com>
2022-10-30 16:24:07 +0300

    [add] Result Tests

---
+++ Homework8/Hw8.Tests/ResultTests.cs
@@ -0,0 +1,37 @@
+﻿using Hw8.Calculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Hw8.Tests;
+
+public class ResultTests
+{
+    int OkValue = 42;
+    string ErrorValue = "Error Value";
+
+    [Fact]
+    public void Ok_OkValue_ReturnsSuccessAndCorrectValue()
+    {
+        //act
+        var result = Result<int, string>.Ok(OkValue);
+
+        //assert
+        Assert.True(result.IsSuccess);
+        Assert.Equal(OkValue, result.Value);
+    }
+
+    [Fact]
+    public void Error_ErrorValue_ReturnsSuccess()
+    {
+        //act
+        var result = Result<int, string>.Error(ErrorValue);
+
+        //assert
+        Assert.True(result.IsFailure);
+        Assert.Equal(ErrorValue, result.ErrorValue);
+    }
+}
